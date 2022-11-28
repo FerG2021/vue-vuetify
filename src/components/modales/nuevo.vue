2e97c2e458ddbf3455fe4769d28b69d4bcfbee24
Fernando2022Gonzalez <fernandojaviergonzalez2018@gmail.com>
2022-11-28 10:06:56 -0300

    mejoras en el modal

--- src/components/modales/nuevo.vue
+++ src/components/modales/nuevo.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="text-center">
-    <v-dialog v-model="dialog">
+    <v-dialog v-model="dialog" transition="dialog-top-transition" persistent>
       <v-card>
         <v-card-text>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </v-card-text>
 
-        <v-btn class="btn" @click="cerrar()"> Cerrar </v-btn>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green-darken-1" variant="text" @click="cerrar()">
+            Disagree
+          </v-btn>
+          <v-btn color="green-darken-1" variant="text" @click="cerrar()">
+            Agree
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-dialog>
   </div>
